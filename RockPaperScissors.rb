4c72b2618bd70b375cd64293aa4a12e58ce003fa
Brent Martin <bmart35@gmail.com>
2016-03-09 14:03:28 -0600

    Got player/computer selection boolean working

--- RockPaperScissors.rb
+++ RockPaperScissors.rb
@@ -26,32 +26,6 @@
 @c_paper = false
 @c_scissors = false
 
-
-
-# puts c_rock
-# puts c_paper
-# puts c_scissors
-# def play
-#   if p_rock && c_rock
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- TIE"
-#   elsif p_paper && c_rock
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- PLAYER WINS"
-#   elsif p_scissors && c_rock
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- COMPUTER WINS"
-#   elsif p_rock && c_paper
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- COMPUTER WINS"
-#   elsif p_paper && c_paper
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- TIE"
-#   elsif p_scissors && c_paper
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- PLAYER WINS"
-#   elsif p_rock && c_scissors
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- PLAYER WINS"
-#   elsif p_paper && c_scissors
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- COMPUTER WINS"
-#   elsif p_scissors && c_scissors
-#   puts "Player: #{p_move} vs Computer: #{c_move} ----- TIE"
-# end
-
 def p_move
   puts "Choose (p)aper, (r)ock, or (s)cissors"
   print "> "
@@ -81,9 +55,31 @@ def c_move
   end
 end
 
+def play
+  if @p_rock && @c_rock
+    puts "Player: *ROCK* vs Computer: *ROCK* ----- TIE"
+  elsif @p_paper && @c_rock
+    puts "Player: *PAPER* vs Computer: *ROCK* ----- PLAYER WINS"
+  elsif @p_scissors && @c_rock
+    puts "Player: *SCISSORS* vs Computer: *ROCK* ----- COMPUTER WINS"
+  elsif @p_rock && @c_paper
+    puts "Player: *ROCK* vs Computer: *PAPER* ----- COMPUTER WINS"
+  elsif @p_paper && @c_paper
+    puts "Player: *PAPER* vs Computer: *PAPER* ----- TIE"
+  elsif @p_scissors && @c_paper
+    puts "Player: *SCISSORS* vs Computer: *PAPER* ----- PLAYER WINS"
+  elsif @p_rock && @c_scissors
+    puts "Player: *ROCK* vs Computer: *SCISSORS* ----- PLAYER WINS"
+  elsif @p_paper && @c_scissors
+    puts "Player: *PAPER* vs Computer: *SCISSORS* ----- COMPUTER WINS"
+  elsif @p_scissors && @c_scissors
+    puts "Player: *SCISSORS* vs Computer: *SCISSORS* ----- TIE"
+  end
+end
+
 p_move
 c_move
-
+play
 
 puts
 
@@ -96,5 +92,7 @@ puts
 puts "Computer: Rock is #{@c_rock}"
 puts "Computer: Paper is #{@c_paper}"
 puts "Computer: Scissors is #{@c_scissors}"
+
+
 # game = Game.new
 # game.play
